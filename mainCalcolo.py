4c1866f44d17191fc5b686a65f70e7047a446edf
Davide Bancora <d.bancora@campus.unimib.it>
2023-05-04 17:51:41 +0200

    DAVIDE: - Aggiunti commenti per una migliore lettura del codice

--- mainCalcolo.py
+++ mainCalcolo.py
@@ -7,18 +7,21 @@ import psutil #funziona su windows
 import os
 import matplotlib.pyplot as plt
 
+# Estrae la matrice dal file .mat e la salva nella variabile 'A'
 def load_matrix_from_file(filename):
     mat = scipy.io.loadmat(filename)
     A = mat['Problem'][0, 0]['A']
     A = scipy.sparse.csc_matrix(A)
     return A
 
+# Crea il vettore 'b' a partire dai valori contenuti nella matrice 'A' passata come parametro affinchè la sol. del sistema lineare sia un vettore 'x' composto da tutti 1
 def create_b_vector(A):
     n = A.shape[0]
     b = A.dot(np.ones(n))
     #DEBUG: print(b)
     return b
 
+# Esegue la decomposizione di cholesky della matrice 'A' passata come parametro e calcola la memoria utilizzata durante il processo 
 def cholesky_decomposition(A):
     # Ottieni la memoria usata prima della fattorizzazione di Cholesky
     memoria_iniziale = None
@@ -43,6 +46,8 @@ def cholesky_decomposition(A):
 
     return factor, memoria_utilizzata_chol
 
+
+# Risolve il sistema lineare Ax=b prendendo in ingresso la matrica 'A' fattorizzata con il metodo di Cholesky e il vettore 'b'. Calcola anche la memoria utilizzata durante il processo 
 def solve_linear_system(factor, b):
     # Ottieni la memoria usata prima della fattorizzazione di Cholesky
     memoria_iniziale = None
@@ -70,24 +75,27 @@ def solve_linear_system(factor, b):
     
     return x, memoria_utilizzata_sistemaLin
 
+# Calcola l'errore relativo della soluzione 'x' del sistema lineare cofrontato con un vettore xEsatto (un vettore contente tutti 1)
 def compute_relative_error(x):
     n = len(x)
     x_esatto = np.ones(n)
     errore_relativo = np.linalg.norm(x - x_esatto) / np.linalg.norm(x_esatto)
     return errore_relativo
 
+# Calcola la percentuale di elementi uguali a 0 nella matrice 'A'
 def compute_percent_zeros(A):
     n_nonzero = A.count_nonzero()
     n_total = A.shape[0] * A.shape[1]
     percent_zero = 100 * (1 - (n_nonzero / n_total))
     return percent_zero
 
+# Calcola il numero di elementi diversi da 0 presenti nella matrice A
 def compute_num_nonzeros(A):
     rows, cols = A.nonzero()
     num_nonzeros = len(rows)
     return num_nonzeros
 
-# Funzione che esegue in loop le varie azioni 
+# MainFile
 def process_file(filename):
     print(f"Elaborazione file {filename}")
     A = load_matrix_from_file(os.path.join(cartella, filename))
@@ -120,7 +128,7 @@ def process_file(filename):
     num_nonzeros = compute_num_nonzeros(A)
     return tempo_cholesky, errore_relativo, percent_zero, num_nonzeros, memoria_totale
 
-# definiamo due array per la creazione dei plot
+# definiamo tre array per la creazione dei plot
 tempi_totali = []
 memoria_cholesky = []
 nomi_matrici = []
@@ -128,7 +136,7 @@ nomi_matrici = []
 # cartella contenente i file .mat
 cartella = "./"
 
-# loop sui file nella cartella
+# Esegue il loop dei file contenuti nella cartella e richiama le funzioni necessarie per calolcare la soluzione del problema
 for filename in os.listdir(cartella):   
 
     if filename.endswith(".mat"):
@@ -142,7 +150,7 @@ for filename in os.listdir(cartella):
 # Ordina i tempi di esecuzione in base alla dimensione dei file
 tempi_totali, memoria_cholesky, nomi_matrici = zip(*sorted(zip(tempi_totali, memoria_cholesky, nomi_matrici), key=lambda x: os.path.getsize(os.path.join(cartella, x[2]))))
 
-# Crea il grafico
+# Crea il grafico dei tempi di esecuzione 
 fig, ax1 = plt.subplots(figsize=(10, 5))
 
 color = 'tab:red'
@@ -158,6 +166,8 @@ for i, v in enumerate(tempi_totali):
 ax1.tick_params(axis='y', labelcolor=color)
 ax1.tick_params(axis='x', rotation=90)
 
+
+# Crea il gragfico della memoria utilizzata
 ax2 = ax1.twinx()  # Secondo asse y
 
 color = 'tab:blue'
